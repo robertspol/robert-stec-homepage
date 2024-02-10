78f4a6234750045a75b2f7e381f015e99a633960
Robert Stec <robertspol@gmail.com>
2024-02-10 23:31:09 +0100

    add project to portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,29 @@
-const projectHoverWrappers = document.querySelectorAll('.portfolio__project-hover-wrapper');
-const arrows = document.querySelectorAll('.fa-arrow-up-right-from-square');
+const projectHoverWrappers = document.querySelectorAll(
+    ".portfolio__project-hover-wrapper"
+);
+const arrows = document.querySelectorAll(".fa-arrow-up-right-from-square");
 
 function handleHover() {
-    const hoverMediaQueries = matchMedia('(any-hover: hover)');
+    const hoverMediaQueries = matchMedia("(any-hover: hover)");
 
     if (hoverMediaQueries.matches) {
         arrows.forEach((arrow, i) => {
-            arrow.addEventListener('mouseenter', function () {
-                projectHoverWrappers[i].style.opacity = '.2';
+            arrow.addEventListener("mouseenter", function () {
+                projectHoverWrappers[i].style.opacity = ".2";
             });
 
-            arrow.addEventListener('mouseleave', function () {
-                projectHoverWrappers[i].style.opacity = '1';
+            arrow.addEventListener("mouseleave", function () {
+                projectHoverWrappers[i].style.opacity = "1";
             });
         });
 
-        projectHoverWrappers.forEach(wrapper => {
-            wrapper.addEventListener('mouseenter', function () {
-                this.style.opacity = '.2';
+        projectHoverWrappers.forEach((wrapper) => {
+            wrapper.addEventListener("mouseenter", function () {
+                this.style.opacity = ".2";
             });
 
-            wrapper.addEventListener('mouseleave', function () {
-                this.style.opacity = '1';
+            wrapper.addEventListener("mouseleave", function () {
+                this.style.opacity = "1";
             });
         });
     }
@@ -29,28 +31,43 @@ function handleHover() {
 
 handleHover();
 
-const gamesWrapper = document.querySelector('.portfolio__project-wrapper--games');
-const chatWrapper = document.querySelector('.portfolio__project-wrapper--chat');
-const englishLanguageSchoolWrapper = document.querySelector('.portfolio__project-wrapper--english-language-school');
-const rysiuTaxiStrzegomWrapper = document.querySelector('.portfolio__project-wrapper--rysiu-taxi-strzegom');
-const rozrywkolandWrapper = document.querySelector('.portfolio__project-wrapper--rozrywkoland');
+const magicOfScienceWrapper = document.querySelector(
+    ".portfolio__project-wrapper--magic-of-science"
+);
+const gamesWrapper = document.querySelector(
+    ".portfolio__project-wrapper--games"
+);
+const chatWrapper = document.querySelector(".portfolio__project-wrapper--chat");
+const englishLanguageSchoolWrapper = document.querySelector(
+    ".portfolio__project-wrapper--english-language-school"
+);
+const rysiuTaxiStrzegomWrapper = document.querySelector(
+    ".portfolio__project-wrapper--rysiu-taxi-strzegom"
+);
+const rozrywkolandWrapper = document.querySelector(
+    ".portfolio__project-wrapper--rozrywkoland"
+);
 
-gamesWrapper.addEventListener('click', function () {
-    open('https://github.com/robertspol/games');
+magicOfScienceWrapper.addEventListener("click", function () {
+    open("https://github.com/robertspol/magic-of-science");
 });
 
-chatWrapper.addEventListener('click', function () {
-    open('https://github.com/robertspol/chat');
+gamesWrapper.addEventListener("click", function () {
+    open("https://github.com/robertspol/games");
 });
 
-englishLanguageSchoolWrapper.addEventListener('click', function () {
-    open('https://robertspol.github.io/english-language-school');
+chatWrapper.addEventListener("click", function () {
+    open("https://github.com/robertspol/chat");
 });
 
-rysiuTaxiStrzegomWrapper.addEventListener('click', function () {
-    open('https://robertspol.github.io/rysiu-taxi-strzegom');
+englishLanguageSchoolWrapper.addEventListener("click", function () {
+    open("https://robertspol.github.io/english-language-school");
 });
 
-rozrywkolandWrapper.addEventListener('click', function () {
-    open('https://robertspol.github.io/rozrywkoland');
+rysiuTaxiStrzegomWrapper.addEventListener("click", function () {
+    open("https://robertspol.github.io/rysiu-taxi-strzegom");
+});
+
+rozrywkolandWrapper.addEventListener("click", function () {
+    open("https://robertspol.github.io/rozrywkoland");
 });
--- js/translation.js
+++ js/translation.js
@@ -1,86 +1,120 @@
-const contactsButton = document.querySelector('.top-buttons-wrapper__contacts');
-const translationButton = document.querySelector('.top-buttons-wrapper__translate');
+const contactsButton = document.querySelector(".top-buttons-wrapper__contacts");
+const translationButton = document.querySelector(
+    ".top-buttons-wrapper__translate"
+);
 
-const aboutNameSection = document.querySelector('.about .main__section-name');
-const textAbout = document.querySelector('.about p');
+const aboutNameSection = document.querySelector(".about .main__section-name");
+const textAbout = document.querySelector(".about p");
 
-const projectGamesName = document.querySelector('.portfolio__project-name--games');
+const projectMagicOfScienceName = document.querySelector(
+    ".portfolio__project-name--magic-of-science"
+);
+const projectGamesName = document.querySelector(
+    ".portfolio__project-name--games"
+);
 
-const gamesDescription = document.querySelector('.portfolio__description--games');
-const chatDescription = document.querySelector('.portfolio__description--chat');;
-const englishLanguageSchoolDescription = document.querySelector('.portfolio__description--english-language-school');
-const rysiuTaxiStrzegomDescription = document.querySelector('.portfolio__description--rysiu-taxi-strzegom');
-const rozrywkolandDescription = document.querySelector('.portfolio__description--rozrywkoland');
+const magicOfScienceDescription = document.querySelector(
+    ".portfolio__description--magic-of-science"
+);
+const gamesDescription = document.querySelector(
+    ".portfolio__description--games"
+);
+const chatDescription = document.querySelector(".portfolio__description--chat");
+const englishLanguageSchoolDescription = document.querySelector(
+    ".portfolio__description--english-language-school"
+);
+const rysiuTaxiStrzegomDescription = document.querySelector(
+    ".portfolio__description--rysiu-taxi-strzegom"
+);
+const rozrywkolandDescription = document.querySelector(
+    ".portfolio__description--rozrywkoland"
+);
 
-const contactsSection = document.querySelector('.contacts .main__section-name');
-const contactsTextSection = document.querySelector('.contacts__email p');
+const contactsSection = document.querySelector(".contacts .main__section-name");
+const contactsTextSection = document.querySelector(".contacts__email p");
 
 const sendernameLabel = document.querySelector('label[for="sendername"]');
 const subjectLabel = document.querySelector('label[for="subject"]');
 const messageLabel = document.querySelector('label[for="message"]');
-const emailButton = document.querySelector('.contacts__button');
+const emailButton = document.querySelector(".contacts__button");
 
 let translation = false;
 
 const translateIntoEnglish = () => {
-    contactsButton.textContent = 'Contacts';
-    translationButton.textContent = 'Przetłumacz na polski';
+    contactsButton.textContent = "Contacts";
+    translationButton.textContent = "Przetłumacz na polski";
 
-    contactsButton.setAttribute('title', 'Contact me');
-    translationButton.setAttribute('title', 'Translate into Polish');
+    contactsButton.setAttribute("title", "Contact me");
+    translationButton.setAttribute("title", "Translate into Polish");
 
-    aboutNameSection.textContent = 'About me';
-    textAbout.textContent = 'Welcome to my website! My name is Robert Stec, and I am a programming enthusiast. I have always been a humanities person eager to explore the world and define it through text. However, over time, I decided to expand my horizons into the realm of exact sciences and began writing text in the form of code. Programming is control over an incredibly powerful machine, which is a computer, and I am constantly developing my skills to deliver the best possible product to anyone who chooses to get in touch with me and use my services. I recommend myself and thank you for visiting my site!';
+    aboutNameSection.textContent = "About me";
+    textAbout.textContent =
+        "Welcome to my website! My name is Robert Stec, and I am a programming enthusiast. I have always been a humanities person eager to explore the world and define it through text. However, over time, I decided to expand my horizons into the realm of exact sciences and began writing text in the form of code. Programming is control over an incredibly powerful machine, which is a computer, and I am constantly developing my skills to deliver the best possible product to anyone who chooses to get in touch with me and use my services. I recommend myself and thank you for visiting my site!";
 
-    projectGamesName.textContent = 'Games';
+    projectMagicOfScienceName.textContent = "Magic Of Science";
+    projectGamesName.textContent = "Games";
 
-    gamesDescription.textContent = 'Two games with the option to create an account and save game progress';
-    chatDescription.textContent = 'Online messenger';
-    englishLanguageSchoolDescription.textContent = 'Website of a non-existent English language school with an interactive dictionary containing basic expressions and an automated gallery with the option for manual operation';
-    rysiuTaxiStrzegomDescription.textContent = "Taxi driver's website business card";
-    rozrywkolandDescription.textContent = 'Website business card for a company offering entertainment and medical services';
-    contactsSection.textContent = 'Contacts';
-    contactsTextSection.textContent = 'Write me an email';
+    magicOfScienceDescription.textContent =
+        'A Single Page Application (SPA) that consists of 3 sections. The first section provides trivia about prime numbers along with an algorithm for calculating them, the second is a game inspired by the quiz show "Who Wants to Be a Millionaire?" with questions related to the world of science, and the third allows downloading space photos from NASA';
+    gamesDescription.textContent =
+        "Two games with the option to create an account and save game progress";
+    chatDescription.textContent = "Online messenger";
+    englishLanguageSchoolDescription.textContent =
+        "Website of a non-existent English language school with an interactive dictionary containing basic expressions and an automated gallery with the option for manual operation";
+    rysiuTaxiStrzegomDescription.textContent =
+        "Taxi driver's website business card";
+    rozrywkolandDescription.textContent =
+        "Website business card for a company offering entertainment and medical services";
+    contactsSection.textContent = "Contacts";
+    contactsTextSection.textContent = "Write me an email";
 
-    sendernameLabel.textContent = 'From';
-    subjectLabel.textContent = 'Subject';
-    messageLabel.textContent = 'Message';
-    emailButton.textContent = 'Send';
-}
+    sendernameLabel.textContent = "From";
+    subjectLabel.textContent = "Subject";
+    messageLabel.textContent = "Message";
+    emailButton.textContent = "Send";
+};
 
 const translateIntoPolish = () => {
-    contactsButton.textContent = 'Kontakt';
-    translationButton.textContent = 'Translate into English';
+    contactsButton.textContent = "Kontakt";
+    translationButton.textContent = "Translate into English";
 
-    contactsButton.setAttribute('title', 'Skontaktuj się ze mną');
-    translationButton.setAttribute('title', 'Przetłumacz na język angielski');
+    contactsButton.setAttribute("title", "Skontaktuj się ze mną");
+    translationButton.setAttribute("title", "Przetłumacz na język angielski");
 
-    aboutNameSection.textContent = 'O mnie';
-    textAbout.textContent = 'Witaj na mojej stronie! Nazywam się Robert Stec i jestem pasjonatem programowania. Zawsze byłem humanistą żądnym poznawania świata i definiowania go za pomocą tekstu, jednak z czasem postanowiłem rozwinąć się o nauki ścisłe i rozpocząłem pisać tekst w postaci kodu. Programowanie to władza nad niesamowicie potężną maszyną, jaką jest komputer, a ja nieustannie rozwijam swoje umiejętności, aby dostarczyć jak najlepszy produkt każdemu, kto zdecyduje się na kontakt ze mną i skorzystanie z moich usług. Polecam się i dziękuję za odwiedzenie mojej strony!';
+    aboutNameSection.textContent = "O mnie";
+    textAbout.textContent =
+        "Witaj na mojej stronie! Nazywam się Robert Stec i jestem pasjonatem programowania. Zawsze byłem humanistą żądnym poznawania świata i definiowania go za pomocą tekstu, jednak z czasem postanowiłem rozwinąć się o nauki ścisłe i rozpocząłem pisać tekst w postaci kodu. Programowanie to władza nad niesamowicie potężną maszyną, jaką jest komputer, a ja nieustannie rozwijam swoje umiejętności, aby dostarczyć jak najlepszy produkt każdemu, kto zdecyduje się na kontakt ze mną i skorzystanie z moich usług. Polecam się i dziękuję za odwiedzenie mojej strony!";
 
-    projectGamesName.textContent = 'Gry';
+    projectMagicOfScienceName.textContent = "Magia Nauki";
+    projectGamesName.textContent = "Gry";
 
-    gamesDescription.textContent = 'Dwie gry z możliwością założenia konta oraz zapisywania stanu rozgrywki';
-    chatDescription.textContent = 'Komunikator internetowy';
-    englishLanguageSchoolDescription.textContent = 'Strona internetowa nieistniejącej szkoły języka angielskiego z interaktywnym słownikiem zawierającym podstawowe wyrażenia i zautomatyzowaną galerią z możliwością ręcznego operowania';
-    rysiuTaxiStrzegomDescription.textContent = 'Strona internetowa-wizytówka kierowcy taksówki';
-    rozrywkolandDescription.textContent = 'Strona internetowa-wizytówka firmy oferującej usługi rozrywkowe oraz medyczne';
+    magicOfScienceDescription.textContent =
+        'Aplikacja SPA, która składa się z trzech sekcji. Pierwsza zawiera ciekawostki na temat liczb pierwszych wraz z algorytmem do ich obliczania, druga to gra inspirowana teleturniejem "Milionerzy" z pytaniami związanymi ze światem nauki, a trzecia umożliwia pobieranie zdjęć kosmosu udostępnianych przez NASA';
+    gamesDescription.textContent =
+        "Dwie gry z możliwością założenia konta oraz zapisywania stanu rozgrywki";
+    chatDescription.textContent = "Komunikator internetowy";
+    englishLanguageSchoolDescription.textContent =
+        "Strona internetowa nieistniejącej szkoły języka angielskiego z interaktywnym słownikiem zawierającym podstawowe wyrażenia i zautomatyzowaną galerią z możliwością ręcznego operowania";
+    rysiuTaxiStrzegomDescription.textContent =
+        "Strona internetowa-wizytówka kierowcy taksówki";
+    rozrywkolandDescription.textContent =
+        "Strona internetowa-wizytówka firmy oferującej usługi rozrywkowe oraz medyczne";
 
-    contactsSection.textContent = 'Kontakt';
-    contactsTextSection.textContent = 'Napisz do mnie e-mail';
+    contactsSection.textContent = "Kontakt";
+    contactsTextSection.textContent = "Napisz do mnie e-mail";
 
-    sendernameLabel.textContent = 'Od';
-    subjectLabel.textContent = 'Temat';
-    messageLabel.textContent = 'Wiadomość';
-    emailButton.textContent = 'Wyślij';
-}
+    sendernameLabel.textContent = "Od";
+    subjectLabel.textContent = "Temat";
+    messageLabel.textContent = "Wiadomość";
+    emailButton.textContent = "Wyślij";
+};
 
-if (navigator.language === 'en') {
+if (navigator.language === "en") {
     translation = true;
     englishTranslation();
 }
 
-translationButton.addEventListener('click', () => {
+translationButton.addEventListener("click", () => {
     translation = !translation;
 
     if (translation === true) {
