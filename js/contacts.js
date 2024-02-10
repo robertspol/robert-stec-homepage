acb121d3a380d6442d40389de8fb57c6fcaa2990
Robert Stec <robertspol@gmail.com>
2024-02-10 22:21:26 +0100

    correct contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -1,24 +1,26 @@
-const phoneNumber = document.querySelector('.contacts__phone-icon-wrapper');
+const phoneNumber = document.querySelector(".contacts__phone-icon-wrapper");
 
-phoneNumber.addEventListener('click', () => {
-    window.open('tel:+48669326496');
+phoneNumber.addEventListener("click", () => {
+    window.open("tel:+48669326496");
 });
 
 function changeTextAreaSize() {
-    const landscape = matchMedia("(min-width: 360px) and (orientation: landscape)");
-    const textArea = document.getElementById('message');
+    const landscape = matchMedia(
+        "(min-width: 360px) and (orientation: landscape)"
+    );
+    const textArea = document.getElementById("message");
 
     if (landscape.matches) {
-        textArea.setAttribute('cols', '50');
-        textArea.setAttribute('rows', '10');
+        textArea.setAttribute("cols", "50");
+        textArea.setAttribute("rows", "10");
     } else {
-        textArea.setAttribute('cols', '20');
-        textArea.setAttribute('rows', '10');
+        textArea.setAttribute("cols", "20");
+        textArea.setAttribute("rows", "10");
     }
 }
 
 changeTextAreaSize();
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     changeTextAreaSize();
 });
